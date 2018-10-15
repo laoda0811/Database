0622c2bf5331b2d3508e81ea033c14c59544fc59
wangpoyao <43333189+wangpoyao@users.noreply.github.com>
2018-10-15 22:26:49 +0800

    Add files via upload

--- W6/routes/index.js
+++ W6/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { id: '405418053', name: '陳思樺' });
+    res.render('index', { id: '403110629', name: '王柏堯' });
 });
 
 module.exports = router;
--- W6/routes/users.js
+++ W6/routes/users.js
@@ -6,13 +6,13 @@ router.get('/', function(req, res, next) {
     //res.send('respond with a resource');
     res.json([{
         id: 1,
-        username: "陳思樺"
+        username: "王柏堯"
     }, {
         id: 2,
-        username: "曾昱文"
+        username: "陳思樺"
     }, {
         id: 3,
-        username: "王柏堯"
+        username: "曾昱文"
     }]);
 });
 
